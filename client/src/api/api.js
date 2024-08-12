f51aa1144297c54222787792e7f2fcf1a0079417
gauravbhaskar080 <gauravbhaskar111@gmail.com>
2024-08-12 07:14:48 +0530

    Update Link

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,4 +1,5 @@
-const API_URL = 'http://localhost:5000';
+// const API_URL = 'http://localhost:5000';
+const API_URL = `https://zomato-uikv.onrender.com`;
 
 // Fetching Restaurants data from the backend
 export const fetchRestaurants = async () => {
